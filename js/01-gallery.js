31f9c99796ccc70a56145356035df1b192d95e1d
dymitrbaranowski <dmitribaranowski51@gmail.com>
2023-06-28 17:49:11 +0300

    fox4

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,26 +4,58 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
-const items = [];
+const galleryCardsSet = createGallery(galleryItems);
 
-galleryItems.forEach((element) => {
-  const galleryItem = document.createElement("div");
-  galleryItem.className = "gallery__item";
-  const galleryLink = document.createElement("a");
-  galleryLink.className = "gallery__link";
-  galleryLink.href = element.original;
-  const galleryImage = document.createElement("img");
-  galleryImage.className = "gallery__image";
-  galleryImage.src = element.preview;
-  galleryImage.setAttribute("data-source", element.original);
-  galleryImage.alt = element.description;
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item" style= " box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
+  <a class="gallery__link" href="${original}" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      style= " box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
 
-  galleryItem.append(galleryLink);
-  galleryLink.append(galleryImage);
-  items.push(galleryItem);
-});
+gallery.insertAdjacentHTML("beforeend", galleryCardsSet);
+// gallery.addEventListener("click", selectGalleryEl);
 
-gallery.append(...items);
+// const items = [];
+
+// Використовуємо шабонні рядки і частіше меп
+
+// galleryItems.forEach((element) => {
+//   const galleryItem = document.createElement("div");
+//   galleryItem.className = "gallery__item";
+//   const galleryLink = document.createElement("a");
+//   galleryLink.className = "gallery__link";
+//   galleryLink.href = element.original;
+//   const galleryImage = document.createElement("img");
+//   galleryImage.className = "gallery__image";
+//   galleryImage.src = element.preview;
+//   galleryImage.setAttribute("data-source", element.original);
+//   galleryImage.alt = element.description;
+
+//   galleryItem.append(galleryLink);
+//   galleryLink.append(galleryImage);
+//   items.push(galleryItem);
+// });
+
+// gallery.append(...items);
+
+
+
+
 
 gallery.addEventListener("click", (e) => {
   e.preventDefault();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,22 +5,41 @@ console.log(galleryItems);
 
 
 const gallery = document.querySelector(".gallery");
-const items = [];
+const galleryCardsSet = createGallery(galleryItems);
 
-galleryItems.forEach((element) => {
-  const galleryLink = document.createElement("a");
-  galleryLink.className = "gallery__link";
-  galleryLink.href = element.original;
-  const galleryImage = document.createElement("img");
-  galleryImage.className = "gallery__image";
-  galleryImage.src = element.preview;
-  galleryImage.setAttribute("title", element.description);
-  galleryImage.alt = element.description;
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item" style="border-radius: 4px; background: transparent; box-shadow: none;">
+  <a class="gallery__item" href="${original}" style= "box-shadow: none;" style="border-radius: 4px;">
+  <img class="gallery__image" src="${preview}" alt="${description}" style="border: 1px solid transparent;
+  border-radius: 4px;
+  padding: 5px; background: rgba(255, 255, 255, 0.3); box-shadow: 0px 1px 3px 0px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 2px 1px -1px rgb(0 0 0 / 12%);"/>
+</a>
+</div>`;
+    })
+    .join("");
+}
 
-  galleryLink.append(galleryImage);
-  items.push(galleryLink);
-});
-gallery.append(...items);
+gallery.insertAdjacentHTML("beforeend", galleryCardsSet);
+
+
+// const items = [];
+
+// galleryItems.forEach((element) => {
+//   const galleryLink = document.createElement("a");
+//   galleryLink.className = "gallery__link";
+//   galleryLink.href = element.original;
+//   const galleryImage = document.createElement("img");
+//   galleryImage.className = "gallery__image";
+//   galleryImage.src = element.preview;
+//   galleryImage.setAttribute("title", element.description);
+//   galleryImage.alt = element.description;
+
+//   galleryLink.append(galleryImage);
+//   items.push(galleryLink);
+// });
+// gallery.append(...items);
 
 new SimpleLightbox(".gallery a", {
   captionDelay: 250,
